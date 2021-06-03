521db96d11ca0536621d22c32df9ed614953431b
Anton Chernichkin <chetoony@gmail.com>
2021-06-03 10:58:23 +0300

    Code review and add modal

--- src/js/script.js
+++ src/js/script.js
@@ -50,4 +50,24 @@ $(document).ready(function(){
     togClass ('.catalog-item__link');
     togClass ('.catalog-item__back');
 
+    //Modal
+
+    $('[data-modal = consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thx').fadeOut('slow');
+    });
+
+    // $(".button_buy").on('click', function() {
+    //     $('.overlay, #order').fadeIn('slow');
+    // });
+
+    $(".button_buy").each(function(i) {
+        $(this).on('click', function(){
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
+
   });
